128d36e94bb0906d050993ff30e5845113d15faa
kishansoya511 <kishangadhavi511@gmail.com>
2024-11-27 13:03:57 +0530

    Initial commi

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return <div className="header">
+    <div className="header-contents">
+        <h2>order your favourite foods</h2>
+        <p>Choose from a diverse menu featuring a delectable array of dishes cafted with the finest ingredients and culinary. Satisfy your carvings and elevate your dining experience, one delicious meal at a time.</p>
+        <button>View menu</button>
+    </div>
+  </div>;
+};
+
+export default Header;
